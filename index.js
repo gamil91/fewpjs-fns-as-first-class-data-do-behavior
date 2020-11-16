6c68a38989c7b988235224beb801e62015d992c6
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-16 14:54:34 -0800

    updated solution

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 function greet(timeString){
-  if (timeString.length === 4){
-  timeString = `0${timeString}`
-  }
-  if (timeString < "12:00") {
+  timeString = parseInt(timeString)
+  if (timeString < 12) {
     return "Good Morning"
   }
-  else if (timeString > "17:00"){
+  else if (timeString > 17){
     return "Good Evening"
   }
   else {
@@ -24,6 +22,8 @@ function greet(timeString){
   }
 }
 /* Write your implementation of displayMessage() */
+
+
 function displayMessage(string){
   greeting = document.querySelector('#greeting')
   greeting.innerText = string
